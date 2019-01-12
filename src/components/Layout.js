fcafba15a2d7262888bc82b85603f0b71852e9ba
micheru30 <mitchell.habel30@gmail.com>
2019-01-12 14:34:13 -0700

    added styling as well as Events and functions for chat.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,45 @@
 import React, {Component} from 'react';
+import io from 'socket.io-client';
+import {USER_CONNECTED} from '../Events'
 
+const socketUrl = 'http://192.168.1.138:3231'
 class Layout extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            socket: null,
+            user: null
+        };
+    }
+
+    componentWillMount() {
+        this.initSocket()
+    }
+
+    initSocket = ()=>{
+        const socket = io(socketUrl)
+        socket.on('connect', ()=>{
+            console.log('connected')
+        })
+        this.setState({socket})
+    }
+
+    setUser = (user)=>{
+        const {socket} = this.state
+        socket.emit(USER_CONNECTED, user)
+        this.setState({user})
+    }
+    logout = ()=>{
+        const {socket} = this.state
+        socket.emit(LOGOUT)
+        this.setState({user:null})
+    }
     render() {
         const {title} = this.props
         return (
             <div className='container'>
-                {title}
+                <LoginForm />
             </div>
         );
     }
